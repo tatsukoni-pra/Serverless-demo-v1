fe2e709d73a1d05111f50ca2a5aafebd891347f6
tatsukoni-pra <90994143+tatsukoni-pra@users.noreply.github.com>
2021-11-25 02:05:01 +0900

    [20211124] カスタムランタイムの検証 (#6)
    * feat: 動くまで
    
    * feat: 調整
    
    * feat: arm64を使うようにする
    
    * feat: 微調整
    
    Co-authored-by: tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>

--- thumbnail/handler.go
+++ thumbnail/handler.go
@@ -1,20 +1,72 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+    "bytes"
+    "log"
+    "net/http"
+    "os"
+	"io/ioutil"
 
 	event "thumbnail/event"
 	thumbnailExec "thumbnail/thumbnailExec"
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, events events.SNSEvent) (string, error) {
-	S3TrigerInfo := event.GetS3TrigerInfo(events)
+var(
+	runtimeApiEndpointPrefix string
+)
+
+func init() {
+	// https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/runtimes-api.html
+	runtimeApiEndpointPrefix = "http://" + os.Getenv("AWS_LAMBDA_RUNTIME_API") + "/2018-06-01/runtime/invocation/"
+}
+
+func main() {
+    log.Println("handler started")
+	// イベントループ
+	for {
+        func() {
+            // コンテキスト情報を取得
+            resp, _ := http.Get(runtimeApiEndpointPrefix + "next")
+            defer func() {
+                resp.Body.Close()
+            }()
+
+            // リクエストIDはヘッダに含まれる
+            rId := resp.Header.Get("Lambda-Runtime-Aws-Request-Id")
+            log.Printf("実行中のリクエストID" + rId)
+
+			// 処理本体
+			data, _ := ioutil.ReadAll(resp.Body)
+			_, err := handle(data)
+			if err != nil {
+				http.Post(respErrorEndpoint(rId), "application/json", bytes.NewBuffer([]byte(rId)))
+				log.Fatal(err)
+			}
+
+            // 最終的に真のランタイムに返すコンテンツはInvocation Response APIのリクエストボディに含める。
+            http.Post(respEndpoint(rId), "application/json", bytes.NewBuffer([]byte(rId)))
+        }()
+    }
+}
+
+func handle(payload []byte) (string, error) {
+	// https://github.com/aws/aws-lambda-go/blob/main/lambda/handler.go#L115 とかを参考に。
+	var snsEvent events.SNSEvent
+	if err := json.Unmarshal(payload, &snsEvent); err != nil {
+		log.Fatal(err)
+	}
+	// ここからlambda処理の本体
+	S3TrigerInfo := event.GetS3TrigerInfo(snsEvent)
 	thumbnailExec.ExecThumbnail(S3TrigerInfo.Bucket, S3TrigerInfo.Key)
 	return "", nil
 }
 
-func main() {
-	lambda.Start(HandleRequest)
+func respEndpoint(requestId string) string {
+    return runtimeApiEndpointPrefix + requestId + "/response"
+}
+
+func respErrorEndpoint(requestId string) string {
+    return runtimeApiEndpointPrefix + requestId + "/error"
 }
